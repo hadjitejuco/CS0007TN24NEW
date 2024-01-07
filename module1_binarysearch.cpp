cb6737f61d7a9db7d3e2f3c1140f196138cd7920
Hadji J. Tejuco <hadji.tejuco@gmail.com>
2024-01-08 07:49:25 +0800

    Create module1_binarysearch.cpp

---
+++ module1_binarysearch.cpp
@@ -0,0 +1,59 @@
+
+//binary search
+
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int l, int r, int x);
+
+int main()
+{
+    int arr[]={2,3,4,10,60,70,71}; //array declaration
+    int n = sizeof(arr)/sizeof(arr[0]); //get the size of the array
+    int x; //search value
+    
+    cout << "Enter number to search: ";
+    cin >> x;
+    
+    int result  = binarySearch(arr, 0, n-1, x);
+    
+    if (result == -1)
+        cout << "Element not found in the array "<< endl;
+    else
+        cout << "Element found at index " << result << endl;
+    
+    return 0;
+
+}
+int binarySearch(int arr[], int l, int r, int x)
+{
+    while(l <= r){
+        int m = l + (r-1)/2;
+        
+        //check if x is present in the middle
+        if (arr[m]==x)
+            return m;
+            
+        // if x greater, ignore left half
+        if (arr[m]<x)
+            l = m + 1;
+        
+        // if x is smaller, ignore the right half
+        else
+        
+            r = m - 1;
+        
+    }
+    return -1;
+
+    
+}
+
+
+
+
+
+
+
+
+
